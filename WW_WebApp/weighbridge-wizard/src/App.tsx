fabc0703174d58a00227db957e7234452cf33b25
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-10-21 18:31:10 +0100

    Landing page now takes a prop that enables it to call a function that simulates redirecting of page on log on. Typescript interface to declare the props as a void function also added

--- WW_WebApp/weighbridge-wizard/src/App.tsx
+++ WW_WebApp/weighbridge-wizard/src/App.tsx
@@ -1,13 +1,27 @@
-
+import {useState} from 'react';
 import './App.css';
-import { MuiTypography } from './components/MuiTypography';
+import {MuiTypography} from './components/MuiTypography'; // For debugging and testing my fonts
+import {LandingPage} from './components/LandingPage';
+import {Dashboard} from './components/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <MuiTypography/>
-    </div>
-  );
+    const [currentPage, setCurrentPage] = useState('LandingPage');
+    // The master hook for managing what state the entire application will be in. By default users should pass verifications via login on the landing page.
+
+    // Function called when user passes validation upon log in.
+    const changePageToDashboard = () => {
+        setCurrentPage('Dashboard');
+    }
+
+    // Rendering is on the condition that state === 'name of state'
+    return (
+        <div className="App">
+            {
+            currentPage === 'LandingPage' && <LandingPage changePageToDashboard={changePageToDashboard}/>}
+            {
+            currentPage === 'Dashboard' && <Dashboard/>
+        } </div>
+    );
 }
 
 export default App;
---
+++ WW_WebApp/weighbridge-wizard/src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Dashboard = () => {
+    return (
+        <>
+            <h1>You have arrived at the dashboard! Content coming soon"</h1>
+            <p> How fun !</p>
+
+        </>
+
+    )
+}
