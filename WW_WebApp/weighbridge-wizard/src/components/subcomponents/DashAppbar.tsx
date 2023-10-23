11e60ab7a09f1c3e476919fe14291de9dea2bd6a
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-10-23 20:11:15 +0100

    fully implemented animated setting button, perhaps i could do with seperating it into a new functional component as separation of concerns

--- WW_WebApp/weighbridge-wizard/src/components/subcomponents/DashAppbar.tsx
+++ WW_WebApp/weighbridge-wizard/src/components/subcomponents/DashAppbar.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 /**
  * This Appbar is only for use when the user is logged in
  * And is working. It should only be used for bare minimum functionality 
@@ -12,7 +13,21 @@ interface DashboardProps2 {
     logout: () => void; // Defines the prop as a void function.
     
 }
+
 const DashAppBar: React.FC < DashboardProps2 > = ({logout}) =>{
+
+    const [settingsHover,setSettingsHover] = useState(false);
+    const [signOutHover,setSignOutHover] = useState(false);
+
+    //Sets states for hovering over my settings button , causing the icon to animate
+    const settingsHovering = () =>{setSettingsHover(true)};
+    const settingsNoLongerHovering = () =>{setSettingsHover(false)};
+
+    //Sets animation state on the log out button
+    
+
+
+
   return (
     <AppBar position="static">       
       <Toolbar>
@@ -26,7 +41,15 @@ const DashAppBar: React.FC < DashboardProps2 > = ({logout}) =>{
           >
             Dashboard
         </Typography>
-        <Typography variant="h4" component="div" sx={{ flexGrow: 1,paddingLeft:'12px' }}/> 
+        <Typography variant="h4" component="div" sx={{ flexGrow: 1,paddingLeft:'12px' }}/>
+       
+        <Button  onMouseEnter={settingsHovering} onMouseLeave={settingsNoLongerHovering} color="inherit" >
+            Settings{settingsHover ? (
+             <FontAwesomeIcon icon={faGear} style={{ marginLeft: '6px' }} size='lg' spin />
+            ) : (
+                <FontAwesomeIcon icon={faGear} style={{ marginLeft: '6px' }} size='lg' />  )}
+        </Button>            
+        <Typography variant="h6" component="span" style={{ margin: '0 8px',marginBottom: '5px' }}>|</Typography>
         <Button onClick={logout} color="inherit">Sign Out<FontAwesomeIcon icon={faRightFromBracket} style={{ marginLeft: '6px' }} /></Button> 
       </Toolbar>
     </AppBar>
