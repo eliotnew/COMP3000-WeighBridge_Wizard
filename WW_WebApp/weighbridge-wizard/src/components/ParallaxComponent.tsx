3e441fec91731beb7bd02865d852ad334850eacf
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-11-02 17:23:47 +0000

    All content is there just needs animating correctly

--- WW_WebApp/weighbridge-wizard/src/components/ParallaxComponent.tsx
+++ WW_WebApp/weighbridge-wizard/src/components/ParallaxComponent.tsx
@@ -9,6 +9,9 @@ import pic3 from '../img_Assets/truck3.jpg';
 import tabs from '../img_Assets/tabsBlue.png';
 import manage from '../img_Assets/manageBlue.png';
 import learning from '../img_Assets/learningBlue.png';
+import ai from '../img_Assets/Artificial intelligence-bro.png';
+import cloud from '../img_Assets/cloudGreen.png';
+import loading from '../img_Assets/loadingGreen.png';
 
 
 
@@ -20,10 +23,9 @@ export const ParallaxComponent = () =>{
 
                 <ParallaxLayer offset={1}  speed={0} factor={1} style={{ backgroundImage: `url(${pic1})`, backgroundSize: "cover", backgroundPosition:"center center", filter: "blur(8px)" }}>  
                 </ParallaxLayer>
-                <ParallaxLayer offset={2}  speed={0} factor={1} style={{ backgroundImage: `url(${pic2})`, backgroundSize: "cover", backgroundPosition:"center center", filter: "blur(8px)" }}>
+                <ParallaxLayer offset={2}  speed={0} factor={1} style={{ backgroundImage: `url(${pic3})`, backgroundSize: "cover", backgroundPosition:"center center", filter: "blur(8px)" }}>
                 </ParallaxLayer>
-                <ParallaxLayer offset={3}  speed={0} factor={1} style={{ backgroundImage: `url(${pic3})`, backgroundSize: "cover", backgroundPosition:"center center", filter: "blur(8px)" }}>
-                </ParallaxLayer>
+                
 
 
                 <ParallaxLayer  offset={0} speed={-3} factor={1}  >
@@ -45,22 +47,55 @@ export const ParallaxComponent = () =>{
                 
                 <ParallaxLayer offset={1.2} speed={1} factor={1}>
                     <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.secondary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
-                                <h3> Responsive & Interactive design to create a productive workflow using dynamic tabs to keep up with busy demand.</h3>
+                                
                                 <img src={tabs} alt="Image taken from storyset.com in compliance" style={{ width: '200px', height: '200px' }}/>
+                                <h3> Responsive & Interactive design to create a productive workflow using dynamic tabs to keep up with busy demand.</h3>
                         </Box>    
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.5} speed={1} factor={1}>
                     <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.secondary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
-                                <h3> Features intuitive user friendly design with help and training </h3>
+                                
                                 <img src={learning} alt="Image taken from storyset.com in compliance" style={{ width: '200px', height: '200px' }}/>
+                                <h3> Featuring intuitive user friendly interface with help and training within the application. </h3>
                         </Box>    
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.8} speed={1} factor={1}>
                     <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.secondary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
-                                <h3> Allowing you to manage your weighbridge staff force!</h3>
+                                
                                 <img src={manage} alt="Image taken from storyset.com in compliance" style={{ width: '200px', height: '200px' }}/>
+                                <h3> Making it cheaper and easier for you to manage your weighbridge staff force!</h3>
                         </Box>    
                 </ParallaxLayer>
+
+                <ParallaxLayer offset={2} speed={1} factor={2}>
+                    <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.secondary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
+                                <h1>A weighbridge solution different from others:</h1>
+                        </Box>    
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={2.2} speed={1} factor={1}>
+                    <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.primary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
+                                <h3> AI assisted form completion to speed up processes.</h3>
+                                <img src={ai} alt="Image taken from storyset.com in compliance" style={{ width: '200px', height: '200px' }}/>
+                        </Box>    
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={2.5} speed={1} factor={1}>
+                    <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.primary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
+                                <h3> Cloud based and paperless system to save the trees!</h3>
+                                <img src={cloud} alt="Image taken from storyset.com in compliance" style={{ width: '200px', height: '200px' }}/>
+                        </Box>    
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={2.9} speed={1} factor={1}>
+                    <Box p={2} sx={{ borderRadius: '24px 24px 24px 24px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)', alignItems: 'left', justifyContent: 'center', backgroundColor: theme.palette.primary.main, margin:'35px',width:'50vw', Height:'25vh',textAlign:"left",mx: 'auto', display: 'flex', flexDirection: 'column',color: theme.palette.primary.contrastText, marginTop:'30vh'}}>
+                                
+                                <img src={loading} alt="Image taken from storyset.com in compliance" style={{ width: '200px', height: '200px' }}/>
+                                <h3> An inclusive approach to processing cargo and freight with accessibilty options</h3>
+                        </Box>    
+                </ParallaxLayer>
+
+
 
                 
                 
